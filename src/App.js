9396a7efc94ccabf6812d9fced67ad81a543bbfb
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-03-07 20:29:29 +0100

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Privacy from "./pages/Privacy";
+import Cookies from "./pages/Cookies";
+import Legal from "./pages/Legal";
 
 function App() {
   return (
     <>
-      holi
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/legal' exact element={<Legal />} />
+          <Route path='/cookies' exact element={<Cookies />} />
+          <Route path='/privacy' exact element={<Privacy />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Contact from '../components/Contact'
+import FrontPage from '../components/FrontPage'
+import Gallery from '../components/Gallery'
+import Menu from '../components/Menu'
+
+const Home = () => {
+  return (
+    <>
+      <FrontPage />
+      <Contact />
+      <Menu />
+      <Gallery />
+    </>
+  )
+}
+
+export default Home
